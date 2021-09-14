7c49ff1f757183c2f4f2416ecab954f29cb0a2e6
Alexandritax <silvasolisdelbarrio@gmail.com>
2021-09-13 20:09:15 -0500

    Termino la clase

--- Clase4_1/Javascript/index.js
+++ Clase4_1/Javascript/index.js
@@ -1,3 +1,21 @@
-function clickMeOnClick(){
-    document.write("<h1>Hola mundo</h1>");
+/*function numeroOnClick(numero){
+    const inputNumero = document.getElementById("txt_numeros")
+    inputNumero.value = numero
+}
+//ANTIGUEDAD
+*/
+var memoria, operacion;
+const numeroOnClick = (numero) => {
+    const inputNumero = document.getElementById("txt_numeros");
+    if(inputNumero.value=="0") {
+        inputNumero.value = numero
+    }
+    else inputNumero.value += numero
+}
+
+const signoOnClick = (signo) => {
+    const inputNumero = document.getElementById("txt_numeros");
+    operacion = signo;
+    memoria = parseInt(inputNumero.value);
+    inputNumero.value = "0"
 }
