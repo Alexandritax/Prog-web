ed3c44d5a200db6756718a67de46af04ddc804c3
Alexandritax <silvasolisdelbarrio@gmail.com>
2021-09-21 08:57:43 -0500

    Terminado

--- Clase4_2/Javascript/index.js
+++ Clase4_2/Javascript/index.js
@@ -1,138 +1,166 @@
-/*function numeroOnClick(numero){
-    const inputNumero = document.getElementById("txt_numeros")
-    inputNumero.value = numero
-}
-//ANTIGUEDAD
-*/
-const sumar = (sum1,sum2) => {
+var numeroIngresado;
+var enOperacion = false;
+var operacionActual;
+
+const sumar = (sum1, sum2) => {
     return sum1 + sum2;
 }
 
-const restar = (res1,res2) => {
-    return res1 - res2;
+const restar = (n1, n2) => {
+    return n1 - n2;
 }
 
-const multiplicar = (mul1,mul2) => {
-    return mul1 * mul2;
-}
-
-const dividir = (divi1,divi2) => {
-    return divi1 / divi2;
-}
-
-var numeroIngresado
-var estado = false;
-var simbolo;
-
-/*const numeroOnClick = (numero) => {
-    const inputNumero = document.getElementById("txt_numeros");
-    const numeroStr = inputNumero.value;
-    if(numeroStr == "0" || estado) {
-        inputNumero.value = numero;
-        estado = false;
+const numeroOnClick = (numero) => {
+    const inputNumeros = document.getElementById("txt_numeros")
+    const numeroStr = inputNumeros.value;
+    if (numeroStr == "0" || enOperacion) {
+        inputNumeros.value = numero;
+        enOperacion = false;
+    }else {
+        inputNumeros.value = numeroStr + numero;
     }
-    else inputNumero.value = numeroStr + numero;
-}*/
+}
 
 const numeroOnClickNoObstrusivo = (evt) => {
-    const inputNumero = document.getElementById("txt_numeros");
-    const but=evt.target;
+    const but = evt.target;
     const numero = but.innerHTML;
-    const numeroStr = inputNumero.value;
-    if(numeroStr == "0" || estado) {
-        inputNumero.value = numero;
-        estado = false;
-    }
-    else inputNumero.value = numeroStr + numero;
+    numeroOnClick(numero)
 }
 
-/*const operacionOnClick = (op) => {
-    const inputNumero = document.getElementById("txt_numeros");
-    if (op == "C") {
-        inputNumero.value="0";
-    }
-    else if (op == "+") {
-        const numero = inputNumero.value;
-        numeroIngresado = parseInt(numero);
-        estado = true;
-        simbolo = sumar;
-    }
-    else if (op == "-") {
-        const numero = inputNumero.value;
-        numeroIngresado = parseInt(numero);
-        estado = true;
-        simbolo = restar;
-    }
-    else if (op == "*") {
-        const numero = inputNumero.value;
-        numeroIngresado = parseInt(numero);
-        estado = true;
-        simbolo = multiplicar;
-    }
-    else if (op == "/") {
-        const numero = inputNumero.value;
-        numeroIngresado = parseInt(numero);
-        estado = true;
-        simbolo = dividir;
-    }
-    else if (op == "=") {
-        if (simbolo !=undefined){
-        const numero = inputNumero.value
-        const res = simbolo(numeroIngresado, parseInt(numero))
-        inputNumero.value = res
+const operacionOnClick = (operacion) => {
+    const inputNumeros = document.getElementById("txt_numeros")
+    if (operacion == "C") {
+        inputNumeros.value = "0";
+    }else if (operacion == "+") {
+        const numero = inputNumeros.value
+        numeroIngresado = parseInt(numero)
+        enOperacion = true;
+        operacionActual = sumar;
+    }else if (operacion == "-") {
+        const numero = inputNumeros.value
+        numeroIngresado = parseInt(numero)
+        enOperacion = true;
+        operacionActual = restar;
+    }else if (operacion == "=") {
+        if (operacionActual != undefined) {
+            const numero = inputNumeros.value
+            const res = operacionActual(numeroIngresado, parseInt(numero))
+            inputNumeros.value = res;
         }
     }
-}*/
+}
 
 const operacionOnClickNoObstrusivo = (evt) => {
-    const inputNumero = document.getElementById("txt_numeros");
-    const but = evt.target
-    const op = but.innerHTML
-    if (op == "C") {
-        inputNumero.value="0";
-    }
-    else if (op == "+") {
-        const numero = inputNumero.value;
-        numeroIngresado = parseInt(numero);
-        estado = true;
-        simbolo = sumar;
-    }
-    else if (op == "-") {
-        const numero = inputNumero.value;
-        numeroIngresado = parseInt(numero);
-        estado = true;
-        simbolo = restar;
-    }
-    else if (op == "*") {
-        const numero = inputNumero.value;
-        numeroIngresado = parseInt(numero);
-        estado = true;
-        simbolo = multiplicar;
-    }
-    else if (op == "/") {
-        const numero = inputNumero.value;
-        numeroIngresado = parseInt(numero);
-        estado = true;
-        simbolo = dividir;
-    }
-    else if (op == "=") {
-        if (simbolo !=undefined){
-        const numero = inputNumero.value
-        const res = simbolo(numeroIngresado, parseInt(numero))
-        inputNumero.value = res
+    const but = evt.target;
+    const op = but.innerHTML;
+    operacionOnClick(op)
+}
+
+const crearInputNumeros = () => {
+    var inputNumeros = document.createElement("input")
+    inputNumeros.setAttribute("id", "txt_numeros")
+    inputNumeros.setAttribute("class", "form-control mb-3")
+    inputNumeros.setAttribute("type", "text")
+    inputNumeros.setAttribute("value", "0")
+
+    return inputNumeros
+}
+
+const crearDivFilaBotones = (numFila) => {
+    var div = document.createElement("div")
+    div.setAttribute("class", "row mb-1")
+
+
+    for (var i = 0; i< 4; i++) {
+        var divCol = document.createElement("div")
+        divCol.setAttribute("class", "col")
+
+        var etiqueta = ""
+        if (i == 3) {
+            // Boton operacion
+            switch(numFila) {
+                case 7: 
+                    etiqueta= "+"
+                break;
+                case 4: etiqueta= "-"
+                break;
+                case 1: etiqueta= "*"
+                break;
+            }
+            
+        }else {
+            etiqueta = (numFila + i)
         }
+    
+        var but = document.createElement("button")
+        but.setAttribute("id", "but" + etiqueta)
+        but.setAttribute("class", "btn btn-primary")
+        but.setAttribute("type", "button")
+        but.innerText = etiqueta
+        divCol.appendChild(but)
+        div.appendChild(divCol)
+    }
+    return div;
+}
+
+const crearUltimaFila = () => {
+    const etiquetas = ["C", "0", "=", "/"]
+    var div = document.createElement("div")
+    div.setAttribute("class", "row mb-1")
+
+    for (var etiqueta of etiquetas) {
+        var divCol = document.createElement("div")
+        divCol.setAttribute("class", "col")
+    
+        var but = document.createElement("button")
+        but.setAttribute("id", "but" + etiqueta)
+        but.setAttribute("class", "btn btn-primary")
+        but.setAttribute("type", "button")
+        but.innerText = etiqueta
+
+        divCol.appendChild(but)
+        div.appendChild(divCol)
+    }
+
+    return div
+}
+
+const construirCalculadora = () => {
+    var inputNumeros = crearInputNumeros();
+    document.body.appendChild(inputNumeros);
+
+    var divFila = crearDivFilaBotones(7)
+    document.body.appendChild(divFila);
+
+    divFila = crearDivFilaBotones(4)
+    document.body.appendChild(divFila);
+
+    divFila = crearDivFilaBotones(1)
+    document.body.appendChild(divFila);
+
+    divFila = crearUltimaFila()
+    document.body.appendChild(divFila);
+}
+
+
+
+const main = () => {
+    construirCalculadora();
+
+
+
+    for (var i =0 ; i < 10; i++) {
+        const but = document.getElementById("but" + i)
+        //but.onclick = numeroOnClickNoObstrusivo
+        but.addEventListener("click", numeroOnClickNoObstrusivo)
+    }
+
+    const arrOperaciones = ["+", "-", "C", "="]
+    for (var op of arrOperaciones) {
+        const but = document.getElementById("but" + op)
+        but.setAttribute("billy", "pw")
+        but.onclick = operacionOnClickNoObstrusivo;
     }
 }
 
-const main = () => {
-    for (var i = 0; i < 10; i++){
-        const but = document.getElementById("but_"+ i)
-        but.onclick = numeroOnClickNoObstrusivo;
-    }
-    const arrOperaciones = ['+','-','*','/','c','=']
-    for(var op of arrOperaciones){
-        const but = document.getElementById("but_"+op)
-        but.onclick = operacionOnClickNoObstrusivo
-    }
-}
-windows.addEventListener("load", main);
+window.addEventListener("load", main);
