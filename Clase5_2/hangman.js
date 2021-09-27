89829a3b983cf2572deaf5c087e54964a1493f26
Alexandritax <silvasolisdelbarrio@gmail.com>
2021-09-27 18:54:00 -0500

    Desarrollo funcional

--- Clase5_2/hangman.js
+++ Clase5_2/hangman.js
@@ -30,11 +30,42 @@ const cargarRefran = (refran) => {
     //divrefran.innerText = refran;
 }
 
+const buscarLetterRefran = (letra, refran, refranOculto) => {
+    let nuevoRefranOculto = "";
+    for (let i=0; i< refran.length; i++) {
+        if (letra == refran[i]){
+            //si es la letra
+            //CARRO  _ _ _ _ _
+            // A -> _ A _ _ _ 
+            nuevoRefranOculto += refran[i]
+        }
+        else {
+            nuevoRefranOculto += refranOculto[i]
+            //No es la letra
+        }
+    }
+    return nuevoRefranOculto;
+}
+var Refran = elegirRefran();
+var RefranOculto = ocultarRefran(Refran)
+const letraInputOnKeyPress = (evt) => {
+    const letraIngresada = evt.key.toUpperCase()
+    const nuevoRefranOculto = buscarLetterRefran(letraIngresada, Refran, RefranOculto)
+    if(nuevoRefranOculto == RefranOculto){
+        console.log("Nueva Imagen")
+    }
+    else {
+        RefranOculto = nuevoRefranOculto
+        cargarRefran(RefranOculto);
+    }
+}
+
 const main = () => {
-    const refran = elegirRefran();
-    const refranOculto = ocultarRefran(refran)
-    cargarRefran(refranOculto);
-    console.log(refran)
+    cargarRefran(RefranOculto);
+    //console.log(refran)
+    let inputLetras = document.querySelector("#letras")
+    //console.log(inputLetras)
+    inputLetras.addEventListener("keypress", letraInputOnKeyPress)
 }
 
 window.addEventListener("load",main)
