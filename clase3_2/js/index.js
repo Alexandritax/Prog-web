992458fc1f104ea4f4d8a778342b707f2966ebc1
Alexandritax <silvasolisdelbarrio@gmail.com>
2021-10-04 19:12:35 -0500

    promesa

--- clase3_2/js/index.js
+++ clase3_2/js/index.js
@@ -1,7 +1,8 @@
-const URL = "https://60b83e68b54b0a0017c03380.mockapi.io/banners"
+const URL_BACKEND = "https://60b83e68b54b0a0017c03380.mockapi.io/banners"
 
 const main = () => {
     //Abrir un canal de comunicacion con el servidor
+    fetch(URL_BACKEND)
 }
 
 window.addEventListener("load", main)
