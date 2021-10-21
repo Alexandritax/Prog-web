40a3574be6c21bfcd5ae927712edd05289089650
Alexandritax <silvasolisdelbarrio@gmail.com>
2021-10-20 19:36:36 -0500

    I AM MAKING MAC AND CHEESE AND NOBODY CAN STOP ME!

--- clase3_2/index.js
+++ clase3_2/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const PORT = 5000;
 const app = express();
 
+app.set("view engine", 'ejs')
+
 app.listen(PORT, ()=> {
     console.log("El servidor inicio correctamente");
 })
