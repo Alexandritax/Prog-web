819f924b73b68090d603c48cb3d8c17ad4c4b52b
Alexandritax <silvasolisdelbarrio@gmail.com>
2021-10-20 19:44:53 -0500

    Noice

--- clase3_2/index.js
+++ clase3_2/index.js
@@ -3,8 +3,13 @@ const express = require('express');
 const PORT = 5000;
 const app = express();
 
+app.use(express.static('assets'))
 app.set("view engine", 'ejs')
 
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
 app.listen(PORT, ()=> {
-    console.log("El servidor inicio correctamente");
+    console.log(`El servidor inicio correctamente en el puerto ${PORT}`);
 })
