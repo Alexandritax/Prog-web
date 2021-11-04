bd0224825f472536a13bd58c7c200d0bec646cd4
Alexandritax <silvasolisdelbarrio@gmail.com>
2021-11-03 19:13:08 -0500

    everything is working

--- clase3_2/index.js
+++ clase3_2/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const session = require('express-session');
 const PORT = 5000;
+
 const app = express();
 
 app.use(express.static('assets'))
 app.set("view engine", 'ejs')
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ 
+    secret : "daleu",
+    resave: false,
+    saveUninitialized: false
+}))
 app.get('/', (req, res) => {
     const listaEventos = [
         {
@@ -51,6 +60,33 @@ app.get('/', (req, res) => {
     res.render('index',{
         eventos : listaEventos,
         topPlayers: listaTopPlayers})
+})
+
+app.get('/torneos', (req, res)=> {
+    res.render('torneos')
+})
+
+
+
+app.post('/login', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    if (username == "pw" && password == "123") {
+        // Login correcto
+        req.session.username = username // guardando variable en sesion
+        res.redirect("/torneos")
+    }else {
+        res.redirect('/login')
+    }
+})
+
+app.get('/login', (req, res)=> {
+    if (req.session.username != undefined) {
+        res.redirect('/torneos')
+    }else {
+        res.render('login')
+    }
 })
 
 app.listen(PORT, ()=> {
