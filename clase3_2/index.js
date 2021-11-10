fc89e2603787bc19a117815b76eea0e6bdee42d0
Alexandritax <silvasolisdelbarrio@gmail.com>
2021-11-10 18:31:12 -0500

    CRUD terminado

--- clase3_2/index.js
+++ clase3_2/index.js
@@ -69,7 +69,7 @@ app.get('/torneos', async (req, res) => {
     const timestampActual = new Date().getTime();
     const dif = timestampActual - req.session.lastLogin
 
-    if (dif < 3 * 60 * 60 * 1000) {
+    if (dif >= 3 * 60 * 60 * 1000) {
         req.session.destroy() // Destruyes la sesion
         res.render('/login')
     }
@@ -83,10 +83,68 @@ app.get('/torneos', async (req, res) => {
     }    
 })
 
-app.get("/torneos_new", (req,res) => {
+app.get("/torneos/new", (req,res) => {
     res.render("torneos_new")
 })
 
+app.post("/torneos/new", async (req,res) => {
+    const torneoNombre = req.body.torneo_nombre
+    const torneoFecha = req.body.torneo_fecha;
+    //console.log(req)
+    await db.Torneo.create({
+        nombre :torneoNombre,
+        fecha :torneoFecha,
+        estado: 1
+    });
+    res.redirect("/torneos")
+})
+
+// path parameters: /torneos/modificar/:id
+// query parameters: /torneos/modificar?id=10
+
+app.get("/torneos/update/:id", async (req,res) => {
+    const idTorneo = req.params.id;
+
+    const torneo = await db.Torneo.findOne({
+        where: {
+            id: idTorneo
+        }
+    })
+
+    res.render('torneos_update', {
+        torneo: torneo
+    })
+})
+
+app.get("/torneos/eliminar/:codigo",async (req,res)=>{
+    const idTorneo = req.params.codigo
+    await db.Torneo.destroy({
+        where: {id: idTorneo}
+    })
+    res.redirect("/torneos")
+})
+
+app.post('/torneos/update', async (req, res) => {
+    const idTorneo = req.body.torneo_id
+    const nombre =req.body.torneo_nombre
+    const fecha = req.body.torneo_fecha
+
+    //1. Obtener un torneo con id: idTorneo
+    const torneo = await db.Torneo.findOne({
+        where : {
+            id : idTorneo
+        }
+    })
+    //2. Cambiar su propiedas / campos
+    torneo.nombre = nombre
+    torneo.fecha = fecha
+
+    //3. Guardo/Actualizo en la base de datos
+    await torneo.save()
+
+    res.redirect('/torneos')
+
+})
 
 app.post('/login', (req, res) => {
     const username = req.body.username
